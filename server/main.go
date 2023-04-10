3cb9f4ed8757cd66cec57821939c18a95e26460b
rj-davidson <6ith@rjd.app>
2023-04-09 19:08:09 -0600

    Added Viper and began setting up Fiber <-> DB connection

--- server/main.go
+++ server/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"log"
+	"mapeleven-server/routes"
+)
 
+func setUpRoutes(app *fiber.App) {
+	app.Get("/hello", routes.Hello)
+}
 func main() {
-	fmt.Println("Hello, world.")
+	connStr := "user=mapeleven dbname=mapeleven-dev-db sslmode=verify-full"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db.Stats()
+	app := fiber.New()
+
+	setUpRoutes(app)
+
+	app.Use(cors.New())
+
+	app.Use(func(c *fiber.Ctx) error {
+		return c.SendStatus(404) // => 404 "Not Found"
+	})
+
+	log.Fatal(app.Listen(":8080"))
 }
---
+++ server/routes/routes.go
@@ -0,0 +1,9 @@
+package routes
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func Hello(c *fiber.Ctx) error {
+	return c.SendString("MapEleven Hello")
+}
