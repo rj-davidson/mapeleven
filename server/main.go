4b2b6f532d36cb176de95eaf255d15486188d673
rj-davidson <6ith@rjd.app>
2023-04-11 13:31:50 -0600

    Data passing from back-end to front-end on request

--- server/main.go
+++ server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/spf13/viper"
 	"io/ioutil"
 	"log"
@@ -11,10 +12,18 @@ import (
 
 func main() {
 	app := fiber.New()
+
+	// Add CORS middleware
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:3000",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
+
 	app.Get("/", apitest)
 	fmt.Println("Server is running on port 8080")
 	log.Fatal(app.Listen(":8080"))
 }
+
 func apitest(c *fiber.Ctx) error {
 	viper.SetConfigFile(".env")
 	viper.ReadInConfig()
@@ -35,29 +44,6 @@ func apitest(c *fiber.Ctx) error {
 	fmt.Println(res)
 	fmt.Println(string(body))
 	fmt.Println("Requested Data", viper.GetString("USER"))
-	return c.SendString(string(body))
+	c.Type("application/json", "utf-8")
+	return c.Send(body)
 }
-
-//func mainold() {
-//	// Setup Viper Environment Variables
-//	viper.SetConfigFile(".env")
-//	viper.ReadInConfig()
-//
-//	connStr := "user=" + viper.GetString("USER") + " password=" + viper.GetString("PASSWORD") + " dbname=" + viper.GetString("DB_NAME") + " sslmode=verify-full"
-//	db, err := sql.Open("postgres", connStr)
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//
-//	db.Stats()
-//	app := fiber.New()
-//
-//	app.Use(cors.New())
-//
-//	app.Use(func(c *fiber.Ctx) error {
-//		return c.SendStatus(404) // => 404 "Not Found"
-//	})
-//
-//	basicApiCall()
-//	log.Fatal(app.Listen(":8080"))
-//}
