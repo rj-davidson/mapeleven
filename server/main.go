c2465ff8f4629700a3ce177bcd7d444cf49d80e1
rj-davidson <6ith@rjd.app>
2023-04-15 00:10:06 -0600

    Reset main.go to work without ent

--- server/main.go
+++ server/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"capstone-cs.eng.utah.edu/mapeleven/mapeleven/ent"
-	"context"
 	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
@@ -12,29 +10,29 @@ import (
 	"net/http"
 )
 
-func initEntClient() (*ent.Client, error) {
-	viper.AutomaticEnv()
-	dbUser := viper.GetString("DB_USER")
-	dbPass := viper.GetString("DB_PASS")
-	dbHost := viper.GetString("DB_HOST")
-	dbPort := viper.GetString("DB_PORT")
-	dbName := viper.GetString("DB_NAME")
-
-	connStr := "host=%s port=%s user=%s dbname=%s password=%s sslmode=disable"
-	connStr = fmt.Sprintf(connStr, dbHost, dbPort, dbUser, dbName, dbPass)
-
-	client, err := ent.Open("postgres", connStr)
-	if err != nil {
-		log.Fatalf("failed opening connection to postgres: %v", err)
-	}
-
-	// Run the auto migration tool.
-	if err := client.Schema.Create(context.Background()); err != nil {
-		log.Fatalf("failed creating schema resources: %v", err)
-	}
-
-	return client, nil
-}
+//func initEntClient() (*ent.Client, error) {
+//	viper.AutomaticEnv()
+//	dbUser := viper.GetString("DB_USER")
+//	dbPass := viper.GetString("DB_PASS")
+//	dbHost := viper.GetString("DB_HOST")
+//	dbPort := viper.GetString("DB_PORT")
+//	dbName := viper.GetString("DB_NAME")
+//
+//	connStr := "host=%s port=%s user=%s dbname=%s password=%s sslmode=disable"
+//	connStr = fmt.Sprintf(connStr, dbHost, dbPort, dbUser, dbName, dbPass)
+//
+//	client, err := ent.Open("postgres", connStr)
+//	if err != nil {
+//		log.Fatalf("failed opening connection to postgres: %v", err)
+//	}
+//
+//	// Run the auto migration tool.
+//	if err := client.Schema.Create(context.Background()); err != nil {
+//		log.Fatalf("failed creating schema resources: %v", err)
+//	}
+//
+//	return client, nil
+//}
 
 func main() {
 	// Web App
@@ -46,21 +44,22 @@ func main() {
 		AllowHeaders: "Origin, Content-Type, Accept",
 	}))
 
-	entClient, errorEnt := initEntClient()
-	if errorEnt != nil {
-		log.Fatalf("failed to connect to ent: %v", errorEnt)
-	}
-	defer entClient.Close()
+	//entClient, errorEnt := initEntClient()
+	//if errorEnt != nil {
+	//	log.Fatalf("failed to connect to ent: %v", errorEnt)
+	//}
+	//defer entClient.Close()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return api(c, entClient)
+		return api(c)
 	})
 
 	fmt.Println("Server is running on port 8080")
 	log.Fatal(app.Listen(":8080"))
 }
 
-func api(c *fiber.Ctx, client *ent.Client) error {
+// func api(c *fiber.Ctx, client *ent.Client) error {
+func api(c *fiber.Ctx) error {
 	viper.SetConfigFile(".env")
 	viper.ReadInConfig()
 
