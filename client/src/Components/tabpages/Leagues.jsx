5eec6856a7962006b0f2141f7f5676d8e2e4b9ac
RonnieMT <ronnieronton@me.com>
2023-04-18 15:37:17 -0600

    Made all the tabs have basic words on them.

---
+++ client/src/Components/tabpages/Leagues.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import '../../App.css';
+
+export default function Leagues() {
+    return <h1 className='leagues'>LEAGUES</h1>;
+}
---
+++ client/src/Components/tabpages/Players.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import '../../App.css';
+
+export default function Players() {
+    return <h1 className='players'>PLAYERS</h1>;
+}
