965bbc74ddd25b62caaf66f161e13b6d007c735d
equinlan <u1255186@utah.edu>
2023-04-19 11:07:25 -0600

    Added SpiderChart and RadarChart to Players tab

--- client/src/Components/tabpages/Players.jsx
+++ client/src/Components/tabpages/Players.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import '../../App.css';
 import NavigationBar from "../NavigationBar.jsx";
 import BarChart from "../BarChart.jsx";
+import SpiderChart from "../SpiderChart.jsx";
+import ScatterPlot from "../ScatterPlot.jsx";
 
 function Players() {
     return (
         <div>
             <NavigationBar />
             <BarChart />
+            <ScatterPlot />
+            <SpiderChart />
             <h1 className='players'>PLAYERS</h1>;
         </div>
     );
