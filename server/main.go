9cb8c6c07b290215f8903c0b7d13bfcf7b3ffc77
James Gibb <51032141+jamesrgibb@users.noreply.github.com>
2023-04-19 18:49:10 -0600

    figured out the issues with the data loading, and put init.go in the db directory.

--- server/main.go
+++ server/main.go
@@ -3,18 +3,25 @@ package main
 import (
 	"context"
 	"entgo.io/ent/dialect"
+	"flag"
 	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
 	"log"
-	"mapeleven/controllers"
+	"mapeleven/db"
 	"mapeleven/db/ent"
 	"mapeleven/db/ent/migrate"
-	"mapeleven/models"
 	"mapeleven/routes"
 )
+
+var updateData bool
+
+func init() {
+	flag.BoolVar(&updateData, "update-data", false, "Update data on startup")
+	flag.Parse()
+}
 
 func main() {
 	// Set up Viper configuration
@@ -51,54 +58,15 @@ func main() {
 	}))
 
 	// Initialize data
-	initializeData(client)
+	if updateData {
+		db.InitializeData(client) // Use the exported function
+	}
 
 	// Set up routes
 	SetupRoutes(app, client)
 
 	fmt.Println("Server is running on port 8080")
 	log.Fatal(app.Listen(":8080"))
-}
-
-func initializeData(client *ent.Client) {
-	// Initialize data
-	laLigaID := 140
-	serieAID := 39
-	bundesligaID := 78
-	premierLeagueID := 61
-	ligue1ID := 71
-	majorleaguesoccerID := 253
-	leagueIDs := []int{laLigaID, serieAID, bundesligaID, premierLeagueID, ligue1ID, majorleaguesoccerID}
-
-	// Country Initialization
-	//{
-	//	countryModel := models.NewCountryModel(client)
-	//	countryController := controllers.NewCountryController(countryModel)
-	//	err := countryController.InitializeCountries()
-	//	if err != nil {
-	//		log.Fatal(err)
-	//	}
-	//}
-
-	// League Initialization
-	{
-		leagueModel := models.NewLeagueModel(client)
-		leagueController := controllers.NewLeagueController(leagueModel)
-		err := leagueController.InitializeLeagues(leagueIDs)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-
-	// Team Initialization
-	{
-		teamModel := models.NewTeamModel(client)
-		teamController := controllers.NewTeamController(teamModel)
-		err := teamController.InitializeTeams(leagueIDs)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
 }
 
 func SetupRoutes(app *fiber.App, client *ent.Client) {
