23c58ef36b2184abc81bf03a7d0e8efa8cd0252a
equinlan <u1255186@utah.edu>
2023-04-20 18:14:29 -0600

    Updated players page

--- client/src/Components/Pages/Players/Players.jsx
+++ client/src/Components/Pages/Players/Players.jsx
@@ -4,13 +4,16 @@ import BarChart from "../../DataVis/BarChart.jsx";
 import SpiderChart from "../../DataVis/SpiderChart.jsx";
 import ScatterPlot from "../../DataVis/ScatterPlot.jsx";
 import Layout from "../../Layout/Layout.tsx";
+import './Players.css'
 
 function Players() {
     return (
         <div>
             <Layout>
-                <BarChart />
-                <ScatterPlot />
+                <div className="chartWrapper1">
+                    <BarChart />
+                    <ScatterPlot />
+                </div>
                 <SpiderChart />
             </Layout>
         </div>
