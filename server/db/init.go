f28f466c0d42a12b0f25f4b45c186d0ba40be5cd
rj-davidson <u1272768@gcloud.utah.edu>
2023-04-24 15:19:24 -0600

    Teams now being initialized by Leagues in db/init.go

--- server/db/init.go
+++ server/db/init.go
@@ -1,11 +1,21 @@
 package db
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/spf13/viper"
+	"io/ioutil"
 	"log"
 	"mapeleven/controllers"
 	"mapeleven/db/ent"
 	"mapeleven/models"
+	"net/http"
 )
+
+var countryModel *models.CountryModel
+var leagueModel *models.LeagueModel
+var teamModel *models.TeamModel
 
 func InitializeData(client *ent.Client) {
 	// Initialize data
@@ -15,21 +25,25 @@ func InitializeData(client *ent.Client) {
 	premierLeagueID := 61
 	ligue1ID := 71
 	majorleaguesoccerID := 253
+	//clubIDs := []int{42, 50, 34, 33, 47, 66, 40, 51, 36, 55, 49, 52, 48, 39, 35, 63, 46, 45, 65, 41}
 	leagueIDs := []int{laLigaID, serieAID, bundesligaID, premierLeagueID, ligue1ID, majorleaguesoccerID}
 
-	// Country Initialization
-	{
-		countryModel := models.NewCountryModel(client)
-		countryController := controllers.NewCountryController(countryModel)
-		err := countryController.InitializeCountries()
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
+	// Model Declaration
+	countryModel = models.NewCountryModel(client)
+	leagueModel = models.NewLeagueModel(client)
+	teamModel = models.NewTeamModel(client)
+
+	//// Country Initialization
+	//{
+	//	countryController := controllers.NewCountryController(countryModel)
+	//	err := countryController.InitializeCountries()
+	//	if err != nil {
+	//		log.Fatal(err)
+	//	}
+	//}
 
 	// League Initialization
 	{
-		leagueModel := models.NewLeagueModel(client)
 		leagueController := controllers.NewLeagueController(leagueModel)
 		err := leagueController.InitializeLeagues(leagueIDs)
 		if err != nil {
@@ -39,11 +53,78 @@ func InitializeData(client *ent.Client) {
 
 	// Team Initialization
 	{
-		teamModel := models.NewTeamModel(client)
+		// Request Standings for each league
+		// Initialize empty teams array of type int
+		teamIDs := fetchTeamIDs()
+
+		// Save teams to database
 		teamController := controllers.NewTeamController(teamModel)
-		err := teamController.InitializeTeams(leagueIDs)
+		err := teamController.InitializeTeams(teamIDs)
 		if err != nil {
 			log.Fatal(err)
 		}
 	}
+}
+
+func fetchTeamIDs() []int {
+	type StandingsResponse struct {
+		Response []struct {
+			League struct {
+				Standings [][]struct {
+					Team struct {
+						ID int `json:"id"`
+					} `json:"team"`
+				} `json:"standings"`
+			} `json:"league"`
+		} `json:"response"`
+	}
+
+	// Get league IDs from database
+	leagues, err := leagueModel.ListLeagues(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+	httpClient := &http.Client{}
+
+	// Initialize an empty list of team IDs
+	teamIDs := []int{}
+
+	for _, league := range leagues {
+		leagueID := league.ID
+		url := fmt.Sprintf("https://api-football-v1.p.rapidapi.com/v3/standings?season=2022&league=%d", leagueID)
+
+		req, err := http.NewRequestWithContext(context.Background(), "GET", url, nil)
+		if err != nil {
+			fmt.Println("Error getting teams for league: ", leagueID, " ", err)
+		}
+		req.Header.Add("x-rapidapi-host", "api-football-v1.p.rapidapi.com")
+		req.Header.Add("x-rapidapi-key", viper.GetString("API_KEY"))
+
+		resp, err := httpClient.Do(req)
+		if err != nil {
+			fmt.Println("Error getting teams for league: ", leagueID, " ", err)
+		}
+		defer resp.Body.Close()
+
+		data, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println("Error getting teams for league: ", leagueID, " ", err)
+		}
+
+		// Get Team ID from json response 'data'
+		var response StandingsResponse
+		err = json.Unmarshal(data, &response)
+		if err != nil {
+			fmt.Println("Error getting teams for league: ", leagueID, " ", err)
+		}
+
+		// Get team IDs from response
+		standings := response.Response[0].League.Standings[0]
+
+		for _, teamStanding := range standings {
+			teamIDs = append(teamIDs, teamStanding.Team.ID)
+		}
+	}
+
+	return teamIDs
 }
