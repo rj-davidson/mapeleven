ba5d81e59033ce2f7bf596dc0e7f95cba01ca694
equinlan <u1255186@utah.edu>
2023-09-10 13:13:40 -0600

    Add individual page slugs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,11 @@ import Players from './Components/Pages/Players/Players.jsx';
 import Clubs from './Components/Pages/Clubs/Clubs.jsx';
 import Leagues from './Components/Pages/Leagues/Leagues.jsx';
 import About from './Components/Pages/About/About.tsx';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ThemeProvider } from '@mui/material';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {ThemeProvider} from '@mui/material';
 import dark from './Components/Theme/MapTheme.tsx';
 import Layout from './Components/Layout/Layout.tsx';
+
 function App() {
     return (
         <ThemeProvider theme={dark}>
@@ -16,12 +17,12 @@ function App() {
                 <Router>
                     <Layout>
                         <Routes>
-                            <Route path='/fixtures' element={<FixturesPage />} />
-                            <Route path='/players' element={<Players />} />
-                            <Route path='/clubs' element={<Clubs />} />
-                            <Route path='/leagues' element={<Leagues />} />
-                            <Route path='/about' element={<About />} />
-                            <Route path='/' element={<FixturesPage />} />
+                            <Route path='/fixtures/:slug' element={<FixturesPage/>}/>
+                            <Route path='/players/:slug' element={<Players/>}/>
+                            <Route path='/clubs/:slug' element={<Clubs/>}/>
+                            <Route path='/leagues/:slug' element={<Leagues/>}/>
+                            <Route path='/about' element={<About/>}/>
+                            <Route path='/' element={<FixturesPage/>}/>
                         </Routes>
                     </Layout>
                 </Router>
