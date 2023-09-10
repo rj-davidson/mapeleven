7a3eb11b02b418b2183f7a68325bc450de192e7c
equinlan <u1255186@utah.edu>
2023-09-10 13:30:59 -0600

    Add search bar

--- client/src/Components/Layout/NavigationBarCopy.jsx
+++ client/src/Components/Layout/NavigationBarCopy.jsx
@@ -1,61 +1,17 @@
 import * as React from 'react';
-import {styled, alpha} from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import InputBase from '@mui/material/InputBase';
 import MenuIcon from '@mui/icons-material/Menu';
-import SearchIcon from '@mui/icons-material/Search';
 import {Link} from "react-router-dom";
+import SearchBar from "./SearchBar.jsx"
 
-const Search = styled('div')(({theme}) => ({
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: alpha(theme.palette.common.white, 0.15),
-    '&:hover': {
-        backgroundColor: alpha(theme.palette.common.white, 0.25),
-    },
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-        marginLeft: theme.spacing(1),
-        width: 'auto',
-    },
-}));
-
-const SearchIconWrapper = styled('div')(({theme}) => ({
-    padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-}));
-
-const StyledInputBase = styled(InputBase)(({theme}) => ({
-    color: 'inherit',
-    '& .MuiInputBase-input': {
-        padding: theme.spacing(1, 1, 1, 0),
-        // vertical padding + font size from searchIcon
-        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-        transition: theme.transitions.create('width'),
-        width: '100%',
-        [theme.breakpoints.up('sm')]: {
-            width: '30ch',
-            '&:focus': {
-                width: '40ch',
-            },
-        },
-    },
-}));
-
-export default function SearchAppBar() {
+export default function NavBar() {
     return (
         <Box sx={{flexGrow: 1}}>
-            <AppBar position="fixed" sx={{backgroundColor: 'var(--dark1)', color: 'var(--light0)'}}>
+            <AppBar position="fixed" sx={{backgroundColor: 'var(--dark1)', color: 'var(--light0)', width: '100%'}}>
                 <Toolbar>
                     <IconButton
                         size="large"
@@ -70,7 +26,7 @@ export default function SearchAppBar() {
                         variant="body"
                         noWrap
                         component="div"
-                        sx={{flexGrow: 1, display: {xs: 'none', sm: 'block'}, fontSize: '3rem', fontWeight: '500'}}
+                        sx={{flexGrow: 1, display: {xs: 'none', md: 'block'}, fontSize: '3rem', fontWeight: '500'}}
                     >
                         <Link to='/'>
                             <div style={{display: "inline", color: "var(--light0)", fontWeight: '200'}}>
@@ -81,15 +37,9 @@ export default function SearchAppBar() {
                             </div>
                         </Link>
                     </Typography>
-                    <Search>
-                        <SearchIconWrapper>
-                            <SearchIcon/>
-                        </SearchIconWrapper>
-                        <StyledInputBase
-                            placeholder="Search players, teams, or leagues"
-                            inputProps={{'aria-label': 'search'}}
-                        />
-                    </Search>
+                    <Box sx={{width: '100%', maxWidth: {xs: 'none', md: 500}}}>
+                        <SearchBar/>
+                    </Box>
                 </Toolbar>
             </AppBar>
         </Box>
