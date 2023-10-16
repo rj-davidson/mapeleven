ae3067912cfaa5465e4be78dcf63287628f34798
James Gibb <51032141+jamesrgibb@users.noreply.github.com>
2023-10-16 08:35:10 -0400

    Test commit with the serializer code

--- server/serializer/player_serializer.go
+++ server/serializer/player_serializer.go
@@ -1,10 +1,59 @@
 package serializer
 
+import (
+	"context"
+	"mapeleven/db/ent"
+	"mapeleven/db/ent/player"
+)
+
+type PlayerInfo struct {
+	ApiFootballID int    `json:"apiFootballID"`
+	Name          string `json:"name"`
+	Firstname     string `json:"firstname"`
+	Lastname      string `json:"lastname"`
+	Age           int    `json:"age"`
+	Height        string `json:"height"`
+	Weight        string `json:"weight"`
+	Injured       bool   `json:"injured"`
+	Photo         string `json:"photo"`
+}
+
 type PlayerSerializer struct {
-	slug    string         `json:"slug"`
-	Name    string         `json:"name"`
-	Age     int            `json:"age"`
-	Photo   string         `json:"photo"`
-	Country string         `json:"country"`
-	Team    TeamSerializer `json:"team"`
+	client *ent.Client
+}
+
+func NewPlayerSerializer(client *ent.Client) *PlayerSerializer {
+	return &PlayerSerializer{client: client}
+}
+
+func (ps *PlayerSerializer) getPlayerData(apiFootballID int) (*ent.Player, error) {
+	playerData, err := ps.client.Player.
+		Query().
+		Where(player.ApiFootballIdEQ(apiFootballID)).
+		Only(context.Background())
+
+	if err != nil {
+		return nil, err
+	}
+
+	return playerData, nil
+}
+
+func (ps *PlayerSerializer) SerializePlayer(apiFootballID int) *PlayerInfo {
+	playerData, err := ps.getPlayerData(apiFootballID)
+	if err != nil {
+		return nil
+	}
+
+	return &PlayerInfo{
+		ApiFootballID: playerData.ApiFootballId,
+		Name:          playerData.Name,
+		Firstname:     playerData.Firstname,
+		Lastname:      playerData.Lastname,
+		Age:           playerData.Age,
+		Height:        playerData.Height,
+		Weight:        playerData.Weight,
+		Injured:       playerData.Injured,
+		Photo:         playerData.Photo,
+	}
 }
