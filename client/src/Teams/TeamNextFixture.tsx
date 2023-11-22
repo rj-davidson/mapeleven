d17d36908c25027c7e6a23884cfe6b93bd571832
equinlan <u1255186@utah.edu>
2023-11-22 01:14:39 -0700

    update next fixture

--- client/src/Teams/TeamNextFixture.tsx
+++ client/src/Teams/TeamNextFixture.tsx
@@ -4,6 +4,7 @@ import { Flex } from "../Util/Flex.jsx";
 import { APITeam } from "../Models/api_types";
 import Typography from "@mui/material/Typography";
 import DisplayImage from "../Util/DisplayImage";
+import { Link } from "react-router-dom";
 
 interface TeamNextFixtureProps {
   teamData: APITeam | undefined;
@@ -13,6 +14,7 @@ const TeamNextFixture: React.FC<TeamNextFixtureProps> = ({ teamData }) => {
   let fixtureSlug = "";
   let oppName = "";
   let oppPhoto = "";
+  let oppSlug = "";
   if (teamData && teamData.competitions?.[0].fixtures) {
     const fixtures = teamData.competitions[0].fixtures;
     // Iterate through fixtures to find the first fixture with status "not started"
@@ -23,41 +25,51 @@ const TeamNextFixture: React.FC<TeamNextFixtureProps> = ({ teamData }) => {
         fixtureSlug = fixture.slug;
         oppName = fixture.awayTeam.name.long;
         oppPhoto = fixture.awayTeam.badge;
+        oppSlug = fixture.awayTeam.slug;
         break;
       }
     }
   }
 
   return (
-    <Tile sx={{ alignItems: "center", justifyContent: "space-between" }}>
-      <Flex style={{ flexDirection: "column" }}>
-        <Typography
-          sx={{
-            fontSize: "20px",
-            textAlign: "left",
-            whiteSpace: "nowrap",
-          }}
-        >
-          Next Match
-        </Typography>
-        <Typography
-          sx={{
-            fontSize: "24px",
-            textAlign: "left",
-            whiteSpace: "nowrap",
-            fontWeight: "bold",
-          }}
-        >
-          {oppName}
-        </Typography>
-      </Flex>
-      <DisplayImage
-        src={oppPhoto}
-        alt={oppName}
-        width={"64px"}
-        height={"64px"}
-      />
-    </Tile>
+    <Link to={`/fixtures/${fixtureSlug}`}>
+      <Tile sx={{ alignItems: "center", justifyContent: "space-between" }}>
+        <Flex style={{ flexDirection: "column" }}>
+          <Typography
+            sx={{
+              fontSize: "20px",
+              textAlign: "left",
+              whiteSpace: "nowrap",
+            }}
+          >
+            Next Match
+          </Typography>
+          <Link to={`/teams/${oppSlug}`}>
+            <Typography
+              sx={{
+                fontSize: "24px",
+                textAlign: "left",
+                whiteSpace: "nowrap",
+                fontWeight: "bold",
+                "&:hover": {
+                  textDecoration: "underline", // Add underline on hover
+                },
+              }}
+            >
+              {oppName}
+            </Typography>
+          </Link>
+        </Flex>
+        <Link to={`/teams/${oppSlug}`}>
+          <DisplayImage
+            src={oppPhoto}
+            alt={oppName}
+            width={"64px"}
+            height={"64px"}
+          />
+        </Link>
+      </Tile>
+    </Link>
   );
 };
 
