73fd3055c8b9d79561847d9e3472fc7889195f74
equinlan <u1255186@utah.edu>
2023-11-22 01:07:25 -0700

    next match component

--- client/src/Fixtures/FixturePageSingleDemo.tsx
+++ client/src/Fixtures/FixturePageSingleDemo.tsx
@@ -3,66 +3,42 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Grid from "@mui/material/Grid";
 import Skeleton from "@mui/material/Skeleton";
-import Typography from "@mui/material/Typography";
-import { APIFixtureEvent, APIFixtureTeam } from "../Models/api_types";
+import { useMediaQuery } from "@mui/material";
+import { APIFixtureSet } from "../Models/api_types";
 import FixtureMatchEvents from "./Events/FixtureMatchEvents";
 import FixturePageSingleMain from "./FixturePageSingleMain";
 import TeamIDCardFixturePage from "../Teams/TeamIDCardFixturePage";
-import { Tile } from "../Util/Tile";
+import { Tile } from "../Util/TileTS";
+import FixtureLineup from "./FixtureLineup";
+import FixtureLineupMobile from "./FixtureLineupMobile";
+import FixturePlayerStepperAway from "./FixturePlayerStepperAway";
+import FixturePlayerStepperHome from "./FixturePlayerStepperHome";
+import FixturePlayerStepper from "./FixturePlayerStepper";
 
 const url = import.meta.env.VITE_API_URL;
 
 function FixturePageSingle() {
   const { slug } = useParams();
-
-  const [homeTeamSlug, setHomeTeamSlug] = useState<string>("");
-  const [awayTeamSlug, setAwayTeamSlug] = useState<string>("");
-  const [homeTeam, setHomeTeam] = useState<string>("");
-  const [awayTeam, setAwayTeam] = useState<string>("");
-  const [homeLogo, setHomeLogo] = useState<string>("");
-  const [awayLogo, setAwayLogo] = useState<string>("");
-  const [referee, setReferee] = useState<string>("");
-  const [homeTeamScore, setHomeTeamScore] = useState<number>(0);
-  const [awayTeamScore, setAwayTeamScore] = useState<number>(0);
-  const [minute, setMinute] = useState<number>(0);
-  const [date, setDate] = useState<Date>(new Date());
-  const [timeZone, setTimeZone] = useState<string>("");
+  const isSmallerThanLG = useMediaQuery("(max-width: 1260px)");
+  const [fixtureData, setFixtureData] = useState<APIFixtureSet>();
   const [loading, setLoading] = useState<boolean>(true);
-  const [events, setEvents] = useState<APIFixtureEvent[]>([]);
-  const [_, setTeams] = useState<APIFixtureTeam>();
 
   useEffect(() => {
     const fetchData = () => {
-      fetch(`${url}/fixtures-demo/${slug}`)
-        .then((response) => response.json())
-        .then((jsonData) => {
-          setHomeTeamSlug(jsonData.teams.home.slug);
-          setAwayTeamSlug(jsonData.teams.away.slug);
-          setHomeTeam(jsonData.teams.home.name);
-          setAwayTeam(jsonData.teams.away.name);
-          setHomeLogo(jsonData.teams.home.logo);
-          setAwayLogo(jsonData.teams.away.logo);
-          setReferee(jsonData.fixture.referee);
-          setMinute(jsonData.fixture.elapsed);
-          setHomeTeamScore(jsonData.fixture.homeTeamScore);
-          setAwayTeamScore(jsonData.fixture.awayTeamScore);
-          setLoading(false);
-          setEvents(jsonData.events);
-          setDate(jsonData.fixture.date);
-          setTimeZone(jsonData.fixture.timezone);
-          setTeams(jsonData.teams);
-          console.log("Got data!");
-        })
-        .catch((error) => console.error(error));
+      slug &&
+        fetch(`${url}/fixtures-demo/${slug}`)
+          .then((response) => response.json())
+          .then((jsonData) => {
+            setFixtureData(jsonData as APIFixtureSet);
+            setLoading(false);
+          })
+          .catch();
     };
 
     fetchData();
     const intervalId = setInterval(fetchData, 30000); // 30 seconds in milliseconds
-
-    // Cleanup the interval on component unmount to avoid memory leaks
     return () => clearInterval(intervalId);
   }, [slug]);
-
   return loading ? (
     <Grid container spacing={2} direction="column">
       <Grid item xs={12} sm={12} md={12} lg={12} width="100%">
@@ -85,52 +61,77 @@ function FixturePageSingle() {
   ) : (
     <Grid container spacing={2}>
       <Grid item xs={12} sm={12}>
-        <FixturePageSingleMain
-          homeTeam={homeTeam}
-          awayTeam={awayTeam}
-          homeLogo={homeLogo}
-          awayLogo={awayLogo}
-          referee={referee}
-          homeTeamScore={homeTeamScore}
-          awayTeamScore={awayTeamScore}
-          minute={minute}
-          events={events}
-          date={date}
-          timeZone={timeZone}
-        />
+        <FixturePageSingleMain data={fixtureData} />
       </Grid>
+      {fixtureData?.lineups &&
+        fixtureData.lineups.home &&
+        fixtureData.lineups.home.matchPlayers.length > 0 &&
+        fixtureData.lineups.away &&
+        fixtureData.lineups.away.matchPlayers.length > 0 && (
+          <Grid item xs={12} sm={12}>
+            {isSmallerThanLG ? (
+              <FixtureLineupMobile
+                homePlayers={fixtureData.lineups.home.matchPlayers}
+                awayPlayers={fixtureData.lineups.away.matchPlayers}
+              />
+            ) : (
+              <FixtureLineup
+                homePlayers={fixtureData.lineups.home.matchPlayers}
+                awayPlayers={fixtureData.lineups.away.matchPlayers}
+              />
+            )}
+          </Grid>
+        )}
       <Grid item xs={12} sm={12}>
         <Grid container spacing={2}>
           <Grid item xs={12} sm={3}>
-            <TeamIDCardFixturePage
-              slug={homeTeamSlug}
-              name={homeTeam}
-              badge={homeLogo}
-            />
+            {fixtureData && fixtureData.teams && (
+              <TeamIDCardFixturePage
+                slug={fixtureData.teams.home.slug}
+                name={fixtureData.teams.home.name}
+                badge={fixtureData.teams.home.logo}
+              />
+            )}
+            <div style={{ marginTop: "16px" }}>
+              {fixtureData && fixtureData.teams && fixtureData.events && (
+                <FixturePlayerStepperHome
+                  events={fixtureData.events}
+                  homeTeamName={fixtureData.teams.home.name}
+                />
+              )}
+            </div>
           </Grid>
           <Grid item xs={12} sm={6}>
-            <Tile style={{ flexDirection: "column" }}>
-              {events ? (
-                events.map((event, index) => (
+            <Tile sx={{ flexDirection: "column" }}>
+              {fixtureData?.events &&
+                fixtureData.events.map((event, index) => (
                   <Grid item xs={12} key={index}>
-                    <FixtureMatchEvents
-                      event={event}
-                      awayTeamSlug={awayTeamSlug}
-                    />
+                    {fixtureData.teams && (
+                      <FixtureMatchEvents
+                        event={event}
+                        awayTeamSlug={fixtureData.teams.away.slug}
+                      />
+                    )}
                   </Grid>
-                ))
-              ) : (
-                // Handle the case when events is null or an empty array
-                <Typography variant="body2">No events yet.</Typography>
-              )}
+                ))}
             </Tile>
           </Grid>
           <Grid item xs={12} sm={3}>
-            <TeamIDCardFixturePage
-              slug={awayTeamSlug}
-              name={awayTeam}
-              badge={awayLogo}
-            />
+            {fixtureData && fixtureData.teams && (
+              <TeamIDCardFixturePage
+                slug={fixtureData.teams.away.slug}
+                name={fixtureData.teams.away.name}
+                badge={fixtureData.teams.away.logo}
+              />
+            )}
+            <div style={{ marginTop: "16px" }}>
+              {fixtureData && fixtureData.teams && fixtureData.events && (
+                <FixturePlayerStepperAway
+                  events={fixtureData.events}
+                  awayTeamName={fixtureData.teams.away.name}
+                />
+              )}
+            </div>
           </Grid>
         </Grid>
       </Grid>
