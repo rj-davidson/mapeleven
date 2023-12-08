63c4e3adffc8bd9c594ba9310c212a5b9d29a6c1
RJ D <hello@rjd.app>
2023-12-08 01:42:14 -0700

    Mobile custom for fixture main

--- client/src/Fixtures/FixturePageSingleMain.tsx
+++ client/src/Fixtures/FixturePageSingleMain.tsx
@@ -25,186 +25,242 @@ const FixturePageSingleMain: React.FC<FixturePageSingleMainProps> = ({
       <Grid item xs={12} sm={12}>
         <Tile>
           <Grid container spacing={2} justifyContent="center" padding={"16px"}>
-            <Grid item xs={12} sm={4}>
-              <Box
-                style={{
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-                gap={1}
-              >
-                <DisplayImage
-                  src={data.teams.home.logo}
-                  alt={data.teams.home.name}
-                  width={"100px"}
-                  height={"100px"}
-                />
-                <Link to={`/teams/${data.teams.home.slug}`}>
-                  <Typography
-                    variant="h6"
-                    align="center"
-                    sx={{
-                      fontSize: "42px",
-                      "&:hover": {
-                        textDecoration: "underline",
-                      },
+            <Grid
+              container
+              item
+              xs={12}
+              justifyContent="center"
+              alignItems="center"
+              spacing={isMobile ? 0 : 3}
+            >
+              <Grid item xs={4}>
+                {isMobile ? (
+                  <Box
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      alignItems: "center",
+                      justifyContent: "flex-end",
                     }}
                   >
-                    {data.teams.home.name}
-                  </Typography>
-                </Link>
-              </Box>
-
-              {data.events
-                ? data.events
-                    .filter(
-                      (event) =>
-                        data.teams !== undefined &&
-                        event.team.name === data.teams.home.name &&
-                        event.type === "Goal" &&
-                        event.detail !== "Missed Penalty"
-                    )
-                    .map((event, index) => (
+                    <DisplayImage
+                      src={data.teams.home.logo}
+                      alt={data.teams.home.name}
+                      width="64px"
+                      height="64px"
+                    />
+                    <Link to={`/teams/${data.teams.home.slug}`}>
                       <Typography
-                        variant="subtitle1"
-                        align="center"
-                        key={index}
+                        variant="subtitle2"
+                        sx={{ "&:hover": { textDecoration: "underline" } }}
                       >
-                        <Box
-                          display={"flex-center"}
-                          justifyContent={"center"}
-                          alignItems={"center"}
-                        >
-                          {`${event.player.name} `}{" "}
-                          <Box ml={1}>
-                            {" "}
-                            <EventTime event={event} />
-                          </Box>
-                        </Box>
+                        {data.teams.home.name}
                       </Typography>
-                    ))
-                : undefined}
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              {data.fixture?.status !== "Not Started" && (
-                <>
-                  <Typography
-                    variant="h4"
-                    align="center"
-                    sx={{
-                      fontSize: "42px",
+                    </Link>
+                  </Box>
+                ) : (
+                  <Box
+                    style={{
+                      display: "flex",
+                      justifyContent: "flex-end",
+                      alignItems: "center",
                     }}
+                    gap={1}
                   >
-                    {data.fixture?.homeTeamScore} -{" "}
-                    {data.fixture?.awayTeamScore}
-                  </Typography>
-
-                  <Typography
-                    variant="h6"
-                    align="center"
-                    sx={{
-                      fontSize: "20px",
-                    }}
-                  >
-                    {[
-                      "In Progress",
-                      "First Half",
-                      "Second Half",
-                      "Extra Time",
-                    ].indexOf(data.fixture?.status ?? "") !== -1
-                      ? `${data.fixture?.elapsed}'`
-                      : data.fixture?.status ?? `${data.fixture?.elapsed}'`}
-                  </Typography>
-                </>
-              )}
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <Box
-                style={{
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-                gap={1}
-              >
-                <DisplayImage
-                  src={data.teams.away.logo}
-                  alt={data.teams.away.name}
-                  width={"100px"}
-                  height={"100px"}
-                ></DisplayImage>
-                <Link to={`/teams/${data.teams.away.slug}`}>
-                  <Typography
-                    variant="h6"
-                    align="center"
-                    sx={{
-                      fontSize: "42px",
-                      "&:hover": {
-                        textDecoration: "underline",
-                      },
-                    }}
-                  >
-                    {data.teams.away.name}
-                  </Typography>
-                </Link>
-              </Box>
-              {data.events
-                ? data.events
-                    .filter(
-                      (event) =>
-                        event.type === "Goal" &&
-                        data.teams !== undefined &&
-                        event.team.name === data.teams.away.name &&
-                        event.detail !== "Missed Penalty"
-                    )
-                    .map((event, index) => (
+                    <Link to={`/teams/${data.teams.home.slug}`}>
                       <Typography
-                        variant="subtitle1"
-                        align="center"
-                        key={index}
+                        variant="h4"
+                        sx={{ "&:hover": { textDecoration: "underline" } }}
+                        paddingRight="8px"
                       >
-                        <Box
-                          display={"flex-center"}
-                          justifyContent={"center"}
-                          alignItems={"center"}
-                        >
-                          {`${event.player.name} `}{" "}
-                          <Box ml={1}>
-                            {" "}
-                            <EventTime event={event} />
-                          </Box>
-                        </Box>
+                        {data.teams.home.name}
                       </Typography>
-                    ))
-                : undefined}
+                    </Link>
+                    <DisplayImage
+                      src={data.teams.home.logo}
+                      alt={data.teams.home.name}
+                      width="96px"
+                      height="96px"
+                    />
+                  </Box>
+                )}
+              </Grid>
+
+              <Grid item xs={4}>
+                {data.fixture?.status !== "Not Started" && (
+                  <>
+                    <Typography variant={isMobile ? "h5" : "h4"} align="center">
+                      {data.fixture?.homeTeamScore} -{" "}
+                      {data.fixture?.awayTeamScore}
+                    </Typography>
+
+                    <Typography
+                      variant={isMobile ? "subtitle2" : "h6"}
+                      align="center"
+                      paddingTop={isMobile ? 0.5 : 1}
+                      fontSize={isMobile ? "12px" : undefined}
+                    >
+                      {[
+                        "In Progress",
+                        "First Half",
+                        "Second Half",
+                        "Extra Time",
+                      ].indexOf(data.fixture?.status ?? "") !== -1
+                        ? `${data.fixture?.elapsed}'`
+                        : data.fixture?.status ?? `${data.fixture?.elapsed}'`}
+                    </Typography>
+                  </>
+                )}
+              </Grid>
+
+              <Grid item xs={4}>
+                {isMobile ? (
+                  <Box
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      alignItems: "center",
+                      justifyContent: "flex-start",
+                    }}
+                  >
+                    <DisplayImage
+                      src={data.teams.away.logo}
+                      alt={data.teams.away.name}
+                      width="64px"
+                      height="64px"
+                    />
+                    <Link to={`/teams/${data.teams.away.slug}`}>
+                      <Typography
+                        variant="subtitle2"
+                        sx={{ "&:hover": { textDecoration: "underline" } }}
+                      >
+                        {data.teams.away.name}
+                      </Typography>
+                    </Link>
+                  </Box>
+                ) : (
+                  <Box
+                    style={{
+                      display: "flex",
+                      justifyContent: "flex-start",
+                      alignItems: "center",
+                    }}
+                    gap={1}
+                  >
+                    <DisplayImage
+                      src={data.teams.away.logo}
+                      alt={data.teams.away.name}
+                      width="96px"
+                      height="96px"
+                    />
+                    <Link to={`/teams/${data.teams.away.slug}`}>
+                      <Typography
+                        variant="h4"
+                        sx={{ "&:hover": { textDecoration: "underline" } }}
+                        paddingLeft="8px"
+                      >
+                        {data.teams.away.name}
+                      </Typography>
+                    </Link>
+                  </Box>
+                )}
+              </Grid>
             </Grid>
-            <Grid item xs={12} sm={12} sx={{ textAlign: "center" }}>
-              <Typography variant="h6" align="center">
-                {data.fixture &&
-                  new Date(data.fixture.date.toString()).toDateString()}
-                {data.fixture &&
-                  data.fixture.status === "Not Started" &&
-                  ` - ${new Date(
-                    data.fixture.date.toString()
-                  ).toLocaleTimeString([], {
-                    hour: "2-digit",
-                    minute: "2-digit",
-                  })}`}
-              </Typography>
-            </Grid>
-            <Grid item xs={12} sm={12}>
-              <Typography
-                variant="subtitle1"
-                align="center"
-                sx={{
-                  fontSize: "16px",
-                }}
-              >
-                <Box gap={1} display="inline-flex" alignItems="center">
-                  <Sports sx={{ fontSize: "24px" }} /> {data.fixture?.referee}
-                </Box>
-              </Typography>
+            <Grid
+              container
+              item
+              xs={12}
+              justifyContent="space-between"
+              alignItems="flex-start"
+            >
+              <Grid item xs={12} sm={4} paddingRight={16}>
+                {data.events && !isMobile
+                  ? data.events
+                      .filter(
+                        (event) =>
+                          data.teams !== undefined &&
+                          event.team.name === data.teams.home.name &&
+                          event.type === "Goal" &&
+                          event.detail !== "Missed Penalty"
+                      )
+                      .map((event, index) => (
+                        <Typography
+                          variant="subtitle1"
+                          align="right"
+                          key={index}
+                        >
+                          <Box
+                            display="flex"
+                            justifyContent="flex-end"
+                            alignItems="center"
+                          >
+                            {`${event.player.name} `}{" "}
+                            <Box ml={1}>
+                              <EventTime event={event} />
+                            </Box>
+                          </Box>
+                        </Typography>
+                      ))
+                  : undefined}
+              </Grid>
+
+              <Grid item xs={12} sm={4}>
+                <Typography
+                  variant={isMobile ? "subtitle2" : "h6"}
+                  align="center"
+                >
+                  {data.fixture &&
+                    new Date(data.fixture.date.toString()).toDateString()}
+                  {data.fixture &&
+                    data.fixture.status === "Not Started" &&
+                    ` - ${new Date(
+                      data.fixture.date.toString()
+                    ).toLocaleTimeString([], {
+                      hour: "2-digit",
+                      minute: "2-digit",
+                    })}`}
+                </Typography>
+                <Typography
+                  variant={isMobile ? "body2" : "subtitle1"}
+                  align="center"
+                >
+                  <Box gap={1} display="inline-flex" alignItems="center">
+                    <Sports sx={{ fontSize: isMobile ? "16px" : "24px" }} />{" "}
+                    {data.fixture?.referee}
+                  </Box>
+                </Typography>
+              </Grid>
+
+              <Grid item xs={12} sm={4} paddingLeft={16}>
+                {data.events && !isMobile
+                  ? data.events
+                      .filter(
+                        (event) =>
+                          event.type === "Goal" &&
+                          data.teams !== undefined &&
+                          event.team.name === data.teams.away.name &&
+                          event.detail !== "Missed Penalty"
+                      )
+                      .map((event, index) => (
+                        <Typography
+                          variant="subtitle1"
+                          align="left"
+                          key={index}
+                        >
+                          <Box
+                            display="flex"
+                            justifyContent="flex-start"
+                            alignItems="center"
+                          >
+                            {`${event.player.name} `}{" "}
+                            <Box ml={1}>
+                              <EventTime event={event} />
+                            </Box>
+                          </Box>
+                        </Typography>
+                      ))
+                  : undefined}
+              </Grid>
             </Grid>
           </Grid>
         </Tile>
